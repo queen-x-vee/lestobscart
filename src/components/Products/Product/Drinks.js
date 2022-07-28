9158cb41a3a6c61be186cd38e7b19bbea02ed687
queen-x-vee <valentinabuoro4@gmail.com>
2022-07-28 16:34:53 +0100

    removed useless keywords

--- src/components/Products/Product/Drinks.js
+++ src/components/Products/Product/Drinks.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaShoppingCart } from 'react-icons/fa'
 
 const Drinks = (props) => {
   return (
     <>
       <div className='w-1/4 h-45 mx-18  flex flex-col items-center justify-center border-b-2'>
         <div>
-          <img src={props.imgsrc} className='w-20 h-20 rounded-full' />
+          <img src={props.imgsrc} className='w-20 h-20 rounded-full' alt = 'imag' />
         </div>
 
         <div className=' font-mono text-base font-medium '>
